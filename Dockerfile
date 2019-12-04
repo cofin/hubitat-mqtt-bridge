28a88f65fd0e0f1a8ff61111c7d588927d01824a
Cody Fincher <cody.fincher@accenture.com>
2019-12-04 14:08:03 -0600

    exception handling. Restructured so that it works in docker or locally

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
 FROM python:3
 # Create app & config directory
 WORKDIR /app
-RUN mkdir -p /app/config
-
+RUN mkdir -p /app/src
 # Install app dependencies
 COPY src/requirements.txt ./
 
 RUN pip install -r requirements.txt
 
 # Bundle app source
-COPY src /app
+COPY src /app/src
 
 # Install config
-COPY config/config.yaml /app/config/
+RUN mkdir -p /app/config
+#COPY config/config.yaml /app/config/
 
-CMD [ "python", "hubitat-mqtt.py" ]
+CMD [ "python", "/app/src/hubitat-mqtt.py" ]
--- src/hubitat-mqtt.py
+++ src/hubitat-mqtt.py
@@ -7,14 +7,18 @@ logger.info('booting application')
 
 
 if __name__ == "__main__":
-    logger.info('starting services')
+    try:
+        logger.info('starting services')
 
-    loop = asyncio.get_event_loop()
-    event_queue = asyncio.Queue()
-    cmd_queue = asyncio.Queue()
-    api = HubitatApi()
-    loop.create_task(api.publish_homeassistant_commands(cmd_queue))
-    loop.create_task(api.send_commands_to_hubitat(cmd_queue, event_queue))
-    loop.create_task(api.publish_hubitat_events(event_queue))
-    loop.create_task(api.send_events_to_mqtt(event_queue))
-    loop.run_forever()
+        loop = asyncio.get_event_loop()
+        event_queue = asyncio.Queue()
+        cmd_queue = asyncio.Queue()
+        api = HubitatApi()
+        loop.create_task(api.publish_homeassistant_commands(cmd_queue))
+        loop.create_task(api.send_commands_to_hubitat(cmd_queue, event_queue))
+        loop.create_task(api.publish_hubitat_events(event_queue))
+        loop.create_task(api.send_events_to_mqtt(event_queue))
+        loop.run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        logger.info('user requested shutdown. terminating')
+        loop.stop()
